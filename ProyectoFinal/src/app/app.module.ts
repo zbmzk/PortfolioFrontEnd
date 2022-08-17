f6f265ee2e782cbeed1b364bc0deed453d305660
zbmzk <kevinsalcedo@hotmail.com>
2022-08-17 02:21:24 -0300

    Componentes basicos con bootstrap funcionando

---
+++ ProyectoFinal/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoCursoComponent } from './components/logo-curso/logo-curso.component';
+import { LogoSocialMediaComponent } from './components/logo-social-media/logo-social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoCursoComponent,
+    LogoSocialMediaComponent,
+    BannerComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
