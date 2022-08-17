4dfb91c6e3a51e6f0bc6fc909b0d90df358221c6
zbmzk <kevinsalcedo@hotmail.com>
2022-08-17 04:31:47 -0300

    Plantilla hasta H&S skills

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +10,9 @@ import { LogoCursoComponent } from './components/logo-curso/logo-curso.component
 import { LogoSocialMediaComponent } from './components/logo-social-media/logo-social-media.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HsskillsComponent } from './components/hsskills/hsskills.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,22 @@ import { AboutComponent } from './components/about/about.component';
     LogoCursoComponent,
     LogoSocialMediaComponent,
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HsskillsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ProyectoFinal/src/app/components/hsskills/hsskills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hsskills',
+  templateUrl: './hsskills.component.html',
+  styleUrls: ['./hsskills.component.css']
+})
+export class HsskillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
