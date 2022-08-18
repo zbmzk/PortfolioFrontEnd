299a64a7174f2c86add5eade66d3413ffe550b91
zbmzk <kevinsalcedo@hotmail.com>
2022-08-18 05:01:54 -0300

    Template hecho, limpia linea por gutter

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -7,36 +7,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoCursoComponent } from './components/logo-curso/logo-curso.component';
-import { LogoSocialMediaComponent } from './components/logo-social-media/logo-social-media.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HsskillsComponent } from './components/hsskills/hsskills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoCursoComponent,
-    LogoSocialMediaComponent,
     BannerComponent,
     AboutComponent,
     ExperienciaComponent,
     EducacionComponent,
     HsskillsComponent,
+    ProyectosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({
-      radius: 100,
-      outerStrokeWidth: 16,
-      innerStrokeWidth: 8,
-      outerStrokeColor: "#78C000",
-      innerStrokeColor: "#C7E596",
-      animationDuration: 300,
-    }),
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
